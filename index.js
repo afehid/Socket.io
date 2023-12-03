62da9e42fc58f98a7e66147cc19f313a765bc386
afahid <a.fehid@elarabygroup.com>
2023-12-03 16:31:23 +0200

    Socket.IO

---
+++ index.js
@@ -0,0 +1,100 @@
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
+
+// open the database file
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database,
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  // create one worker per available core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
+    });
+  }
+  
+  // set up the adapter on the primary thread
+  setupPrimary();
+} else {
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  connectionStateRecovery: {},
+  adapter:createAdapter()
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+//route handlers
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', async (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', async (msg, clientOffset, callback) => {
+    let result;
+    try {
+      result = await db.run(
+        'INSERT INTO messages (content, client_offset) VALUES (?, ?)',
+        msg,
+        clientOffset
+      );
+    } catch (e) {
+      if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+        // the message was already inserted, so we notify the client
+        callback();
+      } else {
+        // nothing to do, just let the client retry
+      }
+      return;
+    }
+    io.emit('chat message', msg, result.lastID);
+    // acknowledge the event
+    callback();
+  });
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each(
+        'SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
+});
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
+}
